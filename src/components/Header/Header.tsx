6f68ec893fddbccabf45dfe98a4a56ca8bcadbb6
ekater1na <ekaterina_l@tut.by>
2023-05-02 17:38:23 +0300

    feat: add sticky header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,31 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './Header.scss';
 import Logo from '../../assets/images/logo.png';
 import { LanguagePicker } from '../LanguagePicker/LanguagePicker';
+import { useAppSelector } from '../../redux/hooks';
+import { RootState } from '../../redux/store';
 
 export const Header: FC = () => {
-  return (
-    <header className="app-container">
-      <div className="logo-wrapper">
-        <img src={Logo} alt="logo" />
+  const isAuth = useAppSelector((state: RootState) => state.user.isAuth);
 
+  const [headerColor, setHeaderColor] = useState<boolean>(false);
+
+  const changeHeaderColor = () => {
+    setHeaderColor(window.scrollY >= 80);
+  };
+
+  window.addEventListener('scroll', changeHeaderColor);
+
+  return (
+    <header className={headerColor ? 'app-container active' : 'app-container'}>
+      <div className="logo-wrapper">
+        <a href="/">
+          <img src={Logo} alt="logo" />
+        </a>
         <LanguagePicker />
       </div>
 
-      <button className="primary-button">{true ? 'Sign In' : 'Sign Up'}</button>
+      <button className="primary-button">{isAuth ? 'Sign In' : 'Sign Up'}</button>
     </header>
   );
 };
