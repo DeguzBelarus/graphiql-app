862bacd8ac56f28924db351b8648179d45a7d0f7
DeguzBelarus <deguz@mail.ru>
2023-05-02 23:50:27 +0300

    feat: add system message component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useRoutes } from './hooks/useRoutes';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import { SystemMessage } from './components/SystemMessage/SystemMessage';
 import './App.scss';
 
 export const App = () => {
@@ -10,6 +11,7 @@ export const App = () => {
       <Header />
       {routes}
       <Footer />
+      <SystemMessage />
     </>
   );
 };
---
+++ src/components/SystemMessage/SystemMessage.tsx
@@ -0,0 +1,55 @@
+import { FC, useEffect, useState } from 'react';
+import { useAppSelector } from '../../redux/hooks';
+
+import { getAuthMessage, getAuthRequestStatus } from '../../redux/slices/userSlice';
+import { SYSTEM_MESSAGE_AUTO_HIDE_DURATION } from '../../constants/constants';
+import { messageClassNameGenerator } from './utils';
+import './SystemMessage.scss';
+
+export const SystemMessage: FC = () => {
+  const authStatus = useAppSelector(getAuthRequestStatus);
+  const authMessage = useAppSelector(getAuthMessage);
+  const [message, setMessage] = useState<string>();
+  const [isShown, setIsShown] = useState(false);
+  const [timer, setTimer] = useState<NodeJS.Timeout>();
+
+  useEffect(() => {
+    const newMessage = authMessage?.message;
+    if (newMessage) {
+      setMessage(newMessage);
+      clearTimeout(timer);
+      setIsShown(true);
+      setTimer(
+        setTimeout(() => {
+          setIsShown(false);
+        }, SYSTEM_MESSAGE_AUTO_HIDE_DURATION)
+      );
+    } else {
+      setIsShown(false);
+      setMessage(undefined);
+    }
+    return () => clearTimeout(timer);
+  }, [authMessage]);
+  if (authStatus !== 'loading' && authMessage) {
+    return (
+      <div
+        className={
+          isShown
+            ? `system-message-wrapper${messageClassNameGenerator(authMessage.severity)} shown`
+            : `system-message-wrapper${messageClassNameGenerator(authMessage.severity)}`
+        }
+      >
+        <p
+          className={
+            isShown
+              ? `message-paragraph${messageClassNameGenerator(authMessage.severity)} shown`
+              : `message-paragraph${messageClassNameGenerator(authMessage.severity)}`
+          }
+        >
+          {message}
+        </p>
+      </div>
+    );
+  }
+  return null;
+};
