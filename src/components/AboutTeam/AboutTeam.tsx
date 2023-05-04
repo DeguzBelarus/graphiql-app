4dbd61d38c8b263234182ef9489bb19faa92e86f
Nataliya Samkevich <nataliyasamkevich@gmail.com>
2023-05-04 12:54:10 +0200

    feat: apply translation to AboutTeam

--- src/components/AboutTeam/AboutTeam.tsx
+++ src/components/AboutTeam/AboutTeam.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { TeamMembersData } from '../../assets/data/members';
 
 import './AboutTeam.scss';
 
 const AboutTeam = () => {
+  const { t } = useTranslation();
+
   return (
     <section className="team">
       <div className="team__wrapper">
-        <h2 className="team__title">Our Team</h2>
+        <h2 className="team__title">{t('ourTeam')}</h2>
         <ul className="team-member__wrapper">
           {TeamMembersData.map((item) => (
             <li
@@ -16,11 +19,11 @@ const AboutTeam = () => {
               onClick={() => (window.location.href = item.url)}
             >
               <div className="team-member__image">
-                <img src={item.imageSrc} alt={item.name} />
+                <img src={item.imageSrc} alt={t(item.name) || ''} />
               </div>
               <div className="team-member__content">
-                <h4 className="team-member__title">{item.name}</h4>
-                <p className="team-member__role">{item.role}</p>
+                <h4 className="team-member__title">{t(item.name)}</h4>
+                <p className="team-member__role">{t(item.role)}</p>
               </div>
             </li>
           ))}
