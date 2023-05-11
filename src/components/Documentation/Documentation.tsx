10a2da322295e45e1b0be0bc445b545cf7da253c
DeguzBelarus <deguz@mail.ru>
2023-05-11 18:00:42 +0300

    feat/refactor: update the documentation component; add loader for documentation

--- src/components/Documentation/Documentation.tsx
+++ src/components/Documentation/Documentation.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useAppSelector } from '../../redux/hooks';
 import { useTranslation } from 'react-i18next';
 
+import { getSchemaRequestStatus } from '../../redux/slices/mainSlice';
+import { RoundLoader } from '../RoundLoader/RoundLoader';
 import './Documentation.scss';
 
 export const Documentation = () => {
   const { t } = useTranslation();
+  const schemaRequestStatus = useAppSelector(getSchemaRequestStatus);
 
   return (
     <div className="docs-wrapper">
-      <h1>{t('docs')}</h1>
-      <div>{t('docsContent')}</div>
+      {schemaRequestStatus === 'loading' ? (
+        <>
+          <h1>{t('docs')}</h1>
+          <RoundLoader />
+        </>
+      ) : (
+        <>
+          <h1>{t('docs')}</h1>
+          <div>{t('docsContent')}</div>
+        </>
+      )}
     </div>
   );
 };
