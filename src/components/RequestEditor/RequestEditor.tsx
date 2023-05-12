3321a0fcbfc5a55192afda2d9081af8b0e2e33dd
ekater1na <ekaterina_l@tut.by>
2023-05-12 15:53:32 +0300

    feat: update textarea

--- src/components/RequestEditor/RequestEditor.tsx
+++ src/components/RequestEditor/RequestEditor.tsx
@@ -1,19 +1,20 @@
-import { FC, useRef } from 'react';
+import React, { FC, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 
 import { setGraphQlQuery, getGraphQlQuery } from '../../redux/slices/mainSlice';
 import './RequestEditor.scss';
+import { Textarea } from '../Textarea/Textarea';
 
 export const RequestEditor: FC = () => {
-  const requestEditorElement = useRef<HTMLTextAreaElement>(null);
+  const [value, setValue] = useState('');
 
   const dispatch = useAppDispatch();
 
   const graphQlQuery = useAppSelector(getGraphQlQuery);
 
-  const graphqlQueryUpdate = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const graphqlQueryUpdate = () => {
     let operationName = '';
-    const query = event.target.value;
+    const query = value;
 
     if (query.split(' ').includes('query')) {
       operationName = query.split(' ')[1];
@@ -21,24 +22,28 @@ export const RequestEditor: FC = () => {
     if (query.split(' ').includes('mutation')) {
       operationName = query.split(' ')[1];
     }
+
     dispatch(
       setGraphQlQuery(
         operationName ? { ...graphQlQuery, operationName, query } : { ...graphQlQuery, query }
       )
     );
   };
+
+  const updateRequest = (value: string) => {
+    setValue(value);
+    graphqlQueryUpdate();
+  };
+
   return (
     <div className="request-editor-wrapper">
-      <textarea
-        className="request-editor-textarea"
-        autoCapitalize="off"
-        autoCorrect="off"
-        autoComplete="off"
-        spellCheck={false}
+      <Textarea
+        name="test-textarea"
+        value={value}
+        onValueChange={(value) => updateRequest(value)}
+        numOfLines={1}
         placeholder="Enter a query"
-        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => graphqlQueryUpdate(event)}
-        ref={requestEditorElement}
-      ></textarea>
+      />
     </div>
   );
 };
--- src/components/VariablesEditor/VariablesEditor.tsx
+++ src/components/VariablesEditor/VariablesEditor.tsx
@@ -1,29 +1,33 @@
-import React, { FC, useRef } from 'react';
+import React, { FC, useState } from 'react';
 import { useAppDispatch } from '../../redux/hooks';
 
 import { setVariablesJSON } from '../../redux/slices/mainSlice';
 import './VariablesEditor.scss';
+import { Textarea } from '../Textarea/Textarea';
 
 export const VariablesEditor: FC = () => {
-  const variablesEditorElement = useRef<HTMLTextAreaElement>(null);
-
   const dispatch = useAppDispatch();
 
-  const graphqlQueryUpdate = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    dispatch(setVariablesJSON(event.target.value));
+  const [value, setValue] = useState('');
+
+  const graphqlQueryUpdate = () => {
+    dispatch(setVariablesJSON(value));
   };
+
+  const updateRequest = (value: string) => {
+    setValue(value);
+    graphqlQueryUpdate();
+  };
+
   return (
     <div className="variables-editor">
-      <textarea
-        className="variables-editor-textarea"
-        autoCapitalize="off"
-        autoCorrect="off"
-        autoComplete="off"
-        spellCheck={false}
+      <Textarea
+        name="test-textarea"
+        value={value}
+        onValueChange={(value) => updateRequest(value)}
+        numOfLines={1}
         placeholder="Enter variables (JSON)"
-        onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => graphqlQueryUpdate(event)}
-        ref={variablesEditorElement}
-      ></textarea>
+      />
     </div>
   );
 };
