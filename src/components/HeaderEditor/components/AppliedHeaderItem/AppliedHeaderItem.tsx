4d65700a9fb6af36c29973839e95c889a8aa3050
DeguzBelarus <deguz@mail.ru>
2023-05-19 18:47:50 +0300

    feat: add message on successfully deleted header with translation

--- src/components/HeaderEditor/components/AppliedHeaderItem/AppliedHeaderItem.tsx
+++ src/components/HeaderEditor/components/AppliedHeaderItem/AppliedHeaderItem.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../redux/hooks';
+import { useTranslation } from 'react-i18next';
 
 import {
   setCurrentRequestHeaders,
   getCurrentRequestHeaders,
+  setSystemMessage,
 } from '../../../../redux/slices/mainSlice';
 import { HeaderEntryArray } from '../../HeaderEditor';
 import { CONTENT_TYPE_HEADER_NAME } from './constants';
@@ -16,6 +18,7 @@ interface Props {
 
 export const AppliedHeaderItem: FC<Props> = ({ headerData }) => {
   const dispatch = useAppDispatch();
+  const { t } = useTranslation();
   const currentRequestHeaders = { ...useAppSelector(getCurrentRequestHeaders) };
 
   const [headerName, headerValue] = headerData;
@@ -23,6 +26,7 @@ export const AppliedHeaderItem: FC<Props> = ({ headerData }) => {
   const removeHeader = () => {
     delete currentRequestHeaders[headerName as keyof HeadersInit];
     dispatch(setCurrentRequestHeaders(currentRequestHeaders));
+    dispatch(setSystemMessage({ message: `${t('headerWasDeleted')}`, severity: 'positive' }));
   };
   return (
     <div className="applied-header-item-wrapper">
